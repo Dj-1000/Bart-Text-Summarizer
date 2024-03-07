85e4bc65c880561af396c1404192165582952e4b
Dheeraj Sharma <dheeraj.sharma@writlabs.com>
2024-03-07 10:16:43 +0530

    Updated the app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import request as req
 import os
 from dotenv import load_dotenv
 load_dotenv()
-app = Flask('__name__')
+app = Flask(__name__)
 app.debug = True
 
 
@@ -26,24 +26,27 @@ def summarize(name = 'Summarize'):
         def query(payload):
             response = requests.post(API_URL, headers=headers, json=payload)
             return response.json()
-
+        output = ''
         data = req.form['data']
-        maxL = int(req.form['maxL'])
-        minL =  maxL 
-        print("MINL :",minL)
-        print("Input data :",data,maxL,minL)
-        # sentence = clean_text(sentence)	
-        print("HI")
-        payload = {
-            "inputs":data ,
-            "parameters":{
-                "max_length": maxL,
-                "min_length": minL
+        if len(data.split(' ')) < 50:
+            error_message ="Sorry! We accept at least 50 words for the summarization."
+        else:
+            maxL = int(req.form['maxL'])
+            minL =  maxL 
+            print("MINL :",minL)
+            print("Input data :",data,maxL,minL)
+            # sentence = clean_text(sentence)	
+            print("HI")
+            payload = {
+                "inputs":data ,
+                "parameters":{
+                    "max_length": maxL,
+                    "min_length": minL
+                }
             }
-        }
-        output = query(payload)[0].get('summary_text')
-        print("OUTPUT :",output)
-        return render_template('index.html', result = output)
+            output = query(payload)[0].get('summary_text')
+            print("OUTPUT :",output)
+        return render_template('index.html', result = output,error = error_message)
     else:
         return redirect(url_for('home'))
 
